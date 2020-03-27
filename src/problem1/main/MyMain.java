79201810c1f469a4b55a1c027d911226278fb906
ritik <ritik.srivastava_cs18@gmail.com>
2020-03-27 13:23:13 +0530

    Successfully compiled.

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -1,11 +1,39 @@
-/*
- *  Created by IntelliJ IDEA.
- *  User: Vaibhav
- *  Date: 23-Mar-20
- *  Time: 7:17 PM
- */
+
 package problem1.main;
 // executable class
 
+import problem1.mybst.MyBinarySearchTree;
+
 public class MyMain {
+    public static void main(String[] args) {
+
+        MyBinarySearchTree tree = new MyBinarySearchTree();
+
+        tree.insert(70);
+        tree.insert(35);
+        tree.insert(93);
+        tree.insert(19);
+        tree.insert(60);
+        tree.insert(79);
+        tree.insert(108);
+        tree.insert(11);
+        tree.insert(28);
+        tree.insert(64);
+        tree.insert(99);
+        tree.insert(111);
+//        tree.printTree(tree.getRootNode());
+
+        tree.printLeftChildrenAndCount(tree.getRootNode());
+
+        tree.insert(5);
+        tree.insert(17);
+        tree.insert(31);
+        tree.insert(3);
+        tree.insert(63);
+        tree.insert(82);
+        tree.insert(106);
+        tree.insert(109);
+        tree.insert(110);
+        tree.printLeftChildrenAndCount(tree.getRootNode());
+    }
 }
--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -1,10 +1,97 @@
-/*
- *  Created by IntelliJ IDEA.
- *  User: Vaibhav
- *  Date: 23-Mar-20
- *  Time: 7:17 PM
- */
+
 package problem1.mybst;
+
+import problem1.node.TreeNode;
+
 // to implement BinarySearchTree
 public class MyBinarySearchTree {
+
+    int count;
+    private TreeNode rootNode;
+
+    public TreeNode getRootNode() {
+        return rootNode;
+    }
+
+    public void insert(int data) {
+        TreeNode node = new TreeNode(data);
+        if (rootNode == null) {
+            rootNode = node;
+        } else {
+            TreeNode temp = rootNode;
+            TreeNode parentNode = null;
+            while (temp != null) {
+                parentNode = temp;
+                if (node.getData() < temp.getData()) {
+                    temp = temp.getLeft();
+                } else {
+                    temp = temp.getRight();
+                }
+            }
+            // setting child node
+            if (node.getData() < parentNode.getData()) {
+                parentNode.setLeft(node);
+            } else {
+                parentNode.setRight(node);
+            }
+        }
+    }
+
+    // will print all element in increasing order
+    private void printTreeInOrder(TreeNode node) {
+        if (node != null) {
+            printTreeInOrder(node.getLeft());
+            System.out.print(node.getData() + " ");
+            printTreeInOrder(node.getRight());
+        }
+    }
+
+    public void printTree(TreeNode node) {
+        System.out.print("Printing tree: ");
+        printTreeInOrder(node);
+    }
+
+    private void printingLeftChild(TreeNode node) {
+        if (node != null) {
+
+            // if node has left child
+            if (node.getLeft() != null) {
+                printingLeftChild(node.getLeft());
+            } else {
+                count++;
+            }
+
+            if (node != rootNode) {
+                System.out.print(node.getData() + " ");
+            }
+
+            // if node has right child
+            if (node.getRight() != null) {
+                reachRightChild(node);
+            }
+        }
+//        System.out.print("ok" + " ");
+    }
+
+    private void reachRightChild(TreeNode node) {
+        node = node.getRight();
+        if (node.getLeft() != null) {
+            printingLeftChild(node.getLeft());
+        } else {
+            count++;
+        }
+        if (node.getRight() != null) {
+            reachRightChild(node);
+        }
+//        System.out.print("works" + " ");
+    }
+
+    public void printLeftChildrenAndCount(TreeNode node) {
+        count = 0;
+        System.out.print("Printing left children: ");
+        printingLeftChild(node);
+        System.out.println();
+        System.out.println("Total nodes with no left child: " + count);
+        System.out.println();
+    }
 }
